e207e4ac7854058c541c4da75fe4098f72f1ae3d
Evgeny Grabun <grabun96@mail.ru>
2023-03-16 13:18:43 +0300

    add: redux files

--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -1,10 +1,30 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BASE_URL } from "../../utils/constants";
 
 const categoriesSlice = createSlice({
   name: "categories",
   initialState: {
-    llist: [],
+    list: [],
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.fulfilled, (state, { payload }) => {
+      state.list = payload;
+    });
   },
 });
 
-export const getCategories = createAsyncThunk();
+export const getCategories = createAsyncThunk(
+  "categories/getCategories",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios(`${BASE_URL}/categories`);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export default categoriesSlice.reducer;
